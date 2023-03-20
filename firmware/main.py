0b5a0c26379b8e05c091ab18153912192f4a7f96
Jorge Pinto <casainho@gmail.com>
2023-03-20 23:19:19 +0000

    Hope to correct issue with graph only update after 48h. Final design. Latest libraries

--- firmware/main.py
+++ firmware/main.py
@@ -1,25 +1,28 @@
 import time
+time_start = time.monotonic()
+
+import supervisor
+import alarm
 import board
 import displayio
 import busio
 import adafruit_ssd1681
 from adafruit_display_text import label
-import alarm
-import supervisor
 import sensor as Sensor
 from circuitpython_uplot.uplot import Uplot, color
 from circuitpython_uplot.ulogging import ulogging
 from adafruit_bitmap_font import bitmap_font
-import alarm
 
-# disable auto-reload
+# disable CircuitPython auto-reload
 supervisor.runtime.autoreload = False
+
+# NOTE: alarm.sleep_memory array size is 4096 bytes on ESP32-S2, CircuitPyhton 8.0.3
+# to keep track of sleep memory we use
+sleep_memory_offset = 0
 
 ###############################################################
 # Let's keep track of number of times we run, by incrementing a counter
-
-# NOTE: alarm.sleep_memory array size is 4096 bytes on ESP32-S2, CircuitPyhton 8.0.3
-
+#
 if not alarm.wake_alarm:
   # reset counter at a power on reset
   counter = 0
@@ -34,48 +37,25 @@ counter += 1
 alarm.sleep_memory[0] = (counter >> 16 & 0xff)
 alarm.sleep_memory[1] = (counter >> 8 & 0xff)
 alarm.sleep_memory[2] = (counter & 0xff)
+
+# here we use the first 3 bytes of sleep memory
+sleep_memory_offset += 3
 ###############################################################
 
 ###############################################################
-# initialize the display
-displayio.release_displays()
-
-spi = busio.SPI(
-    board.IO5, # CLK pin
-    board.IO3, # MOSI pin
-    None) # MISO pin, not need to drive this display
-
-display_bus = displayio.FourWire(
-    spi,
-    command = board.IO9,
-    reset = board.IO11,
-    chip_select = board.IO7,
-    baudrate = 60000000)
-
-display = adafruit_ssd1681.SSD1681(display_bus, width = 200, height = 200, busy_pin = board.IO12, rotation = 0)
-###############################################################
-
-###############################################################
-# initialize the sensor
-sensor = Sensor.Sensor(board.IO37, board.IO35)
-# read the sensor values
-sensor.run_periodic()
-###############################################################
-
+# Prepare all the visuals to display
+#
 g = displayio.Group()
 
-# Set a white background
-# Create the display object - the third color is red (0xff0000)
 DISPLAY_WIDTH = 200
 DISPLAY_HEIGHT = 200
 FOREGROUND_COLOR = color.BLACK
 BACKGROUND_COLOR = color.WHITE
 
 background_bitmap = displayio.Bitmap(DISPLAY_WIDTH, DISPLAY_HEIGHT, 1)
-# Map colors in a palette
 palette = displayio.Palette(1)
 palette[0] = BACKGROUND_COLOR
-# Create a Tilegrid with the background and put in the displayio group
+# create a Tilegrid with the background and put in the displayio group
 t = displayio.TileGrid(background_bitmap, pixel_shader=palette)
 g.append(t)
 
@@ -84,30 +64,100 @@ font_small = bitmap_font.load_font("fonts/Ubuntu-R-12.bdf")
 font_medium = bitmap_font.load_font("fonts/Ubuntu-R-18.bdf")
 font_big = bitmap_font.load_font("fonts/Ubuntu-R-42.bdf")
 
-# numeric values
+# temperature text field
 text_temperature = label.Label(font_big, color=FOREGROUND_COLOR, scale=1)
-text_temperature.anchored_position = 70, 2
+text_temperature.anchored_position = 80, 2
 text_temperature.anchor_point = 1.0, 0.0
 g.append(text_temperature)
 
+# temperature unity text field
 text_temperature_unity = label.Label(font_medium, text='ºC', color=FOREGROUND_COLOR, scale=1)
-text_temperature_unity.anchored_position = 73, 2
-text_temperature_unity.anchor_point = 0.0, -1.3
+text_temperature_unity.anchored_position = 81, 6
+text_temperature_unity.anchor_point = 0.0, -0.9
 g.append(text_temperature_unity)
 
+# humidity text field
 text_humidity = label.Label(font_big, color=FOREGROUND_COLOR, scale=1)
-text_humidity.anchored_position = 160, 2
+text_humidity.anchored_position = 170, 2
 text_humidity.anchor_point = 1.0, 0.0
 g.append(text_humidity)
 
+# humidity unity text field
 text_humidity_unity = label.Label(font_medium, text='%', color=FOREGROUND_COLOR, scale=1)
-text_humidity_unity.anchored_position = 163, 2
-text_humidity_unity.anchor_point = 0.0, -1.3
+text_humidity_unity.anchored_position = 171, 6
+text_humidity_unity.anchor_point = 0.0, -0.9
 g.append(text_humidity_unity)
 
+###############################################################
+# initialize the temperature and humidity sensor
+# make one read and store the values on the sleep memory
+#
+sensor = Sensor.Sensor(i2c_clk_pin = board.IO37, i2c_sda_pin = board.IO35, sleep_memory_offset = sleep_memory_offset)
+sensor.read_and_store()
+###############################################################
+
+# write the temperature value to the text field - also round to have no decimals
 text_temperature.text = f'{int(round(sensor.temperature, 0))}'
+# write the humidty value to the text field - also round to have no decimals
 text_humidity.text = f'{int(round(sensor.humidity, 0))}'
 
+# get the historic data of the sensors from the sleep memory, so it will be displayed on the display
+historic_data_temperature, temperature_max, temperature_min, temperature_len = sensor.historic_data_temperature
+historic_data_humidity, humidity_max, humidity_min, humidity_len = sensor.historic_data_humidity
+
+# calculate the max ranges y for temperature
+if temperature_max > 60:
+  temperature_y_max = temperature_max
+elif temperature_max > 50:
+  temperature_y_max = 60
+elif temperature_max > 40:
+  temperature_y_max = 50
+elif temperature_max > 30:
+  temperature_y_max = 40
+elif temperature_max > 20:
+  temperature_y_max = 30
+elif temperature_max > 10:
+  temperature_y_max = 20
+else:
+  temperature_y_max = 10
+
+# calculate the min ranges y for temperature
+if temperature_min > 60:
+  temperature_y_min = temperature_min
+elif temperature_min > 50:
+  temperature_y_min = 50
+elif temperature_min > 40:
+  temperature_y_min = 40
+elif temperature_min > 30:
+  temperature_y_min = 30
+elif temperature_min > 20:
+  temperature_y_min = 20
+elif temperature_min > 10:
+  temperature_y_min = 10
+else:
+  temperature_y_min = 0
+
+# calculate the max ranges y for humidity
+if humidity_max > 75:
+  humidity_y_max = 100
+elif humidity_max > 50:
+  humidity_y_max = 75
+elif humidity_max > 25:
+  humidity_y_max = 50
+else:
+  humidity_y_max = 25
+
+# calculate the min ranges y for humidity
+if humidity_min > 75:
+  humidity_y_min = 75
+elif humidity_min > 50:
+  humidity_y_min = 50
+elif humidity_min > 25:
+  humidity_y_min = 25
+else:
+  humidity_y_min = 0
+
+# prepare the uplot objects: first graph for temperature and second for humidity
 plot_1 = Uplot(
     21,
     46,
@@ -130,48 +180,23 @@ plot_2 = Uplot(
     background_color=color.WHITE,
     scale=4)
 
-# Setting up tick parameters
+# setting up the uplot tick parameters
 plot_1.tick_params(tickx_height = 5, ticky_height = 5, tickcolor = color.BLACK, tickgrid = False)
 plot_2.tick_params(tickx_height = 5, ticky_height = 5, tickcolor = color.BLACK, tickgrid = False)
 
-# get the historic data of the sensors
-historic_data_temperature, temperature_max, temperature_min, temperature_len = sensor.historic_data_temperature
-historic_data_humidity, humidity_max, humidity_min, humidity_len = sensor.historic_data_humidity
-
-# from random import choice
-# temperatures = list(range(19, 32))
-# humidity = list(range(60, 95))
-# x = list(range(0, 144, 1))
-# historic_data_temperature = [choice(temperatures) for _ in x]
-# temperature_max = 32
-# temperature_min = 0
-# temperature_len = 144
-# historic_data_humidity = [choice(humidity) for _ in x]
-# humidity_max = 95
-
-# calculate the max range y for temperature
-if temperature_max > 55:
-  temperature_range_y = temperature_max
-elif temperature_max > 35:
-  temperature_range_y = 50
-elif temperature_max > 25:
-  temperature_range_y = 40
-elif temperature_max > 15:
-  temperature_range_y = 30
-else:
-  temperature_range_y = 20
-
+# create a list with values for the x axis
 x = list(range(0, temperature_len))
 
+# setting up the ulogging optins
 ulogging(
   plot_1,
   x,
   historic_data_temperature,
   rangex=[0, 143],
-  rangey=[0, temperature_range_y],
+  rangey=[temperature_y_min, temperature_y_max],
   line_color=color.BLACK,
   ticksx=[23, 47, 71, 95, 119, 143],
-  ticksy=[10, 20, 30],
+  ticksy=[int(round(temperature_y_min + (temperature_y_max - temperature_y_min) / 2, 1))],
   fill=True
   )
 
@@ -180,78 +205,98 @@ ulogging(
   x,
   historic_data_humidity,
   rangex=[0, 143],
-  rangey=[0, 100],
+  rangey=[humidity_y_min, humidity_y_max],
   line_color=color.BLACK,
   ticksx=[23, 47, 71, 95, 119, 143],
-  ticksy=[25, 50, 75],
+  ticksy=[int(round(humidity_y_min + (humidity_y_max - humidity_y_min) / 2, 1))],
   fill=True 
   )
 
+# get the two plots appended to the display group
 plot_1.append(plot_2)
 g.append(plot_1)
 
-# graph values
-text_graph_temperature_min = label.Label(font_small, text='0', color=FOREGROUND_COLOR, scale=1)
+# temperature y min field
+text_graph_temperature_min = label.Label(font_small, text=f'{temperature_y_min}', color=FOREGROUND_COLOR, scale=1)
 text_graph_temperature_min.anchor_point = 0.5, 0.0
 text_graph_temperature_min.anchored_position = 9, 100
 g.append(text_graph_temperature_min)
 
-text_graph_temperature_med = label.Label(font_small, text=f'{int(round(temperature_range_y / 2, 1))}', color=FOREGROUND_COLOR, scale=1)
-text_graph_temperature_med.anchor_point = 0.5, 0.0
-text_graph_temperature_med.anchored_position = 9, 74
-g.append(text_graph_temperature_med)
-
-text_graph_temperature_max = label.Label(font_small, text=f'{temperature_range_y}', color=FOREGROUND_COLOR, scale=1)
+# temperature y max field
+text_graph_temperature_max = label.Label(font_small, text=f'{temperature_y_max}', color=FOREGROUND_COLOR, scale=1)
 text_graph_temperature_max.anchor_point = 0.5, 0.0
 text_graph_temperature_max.anchored_position = 9, 48
 g.append(text_graph_temperature_max)
 
-text_graph_humidity_min = label.Label(font_small, text='0', color=FOREGROUND_COLOR, scale=1)
+# humidity y min field
+text_graph_humidity_min = label.Label(font_small, text=f'{humidity_y_min}', color=FOREGROUND_COLOR, scale=1)
 text_graph_humidity_min.anchor_point = 0.5, 0.0
 text_graph_humidity_min.anchored_position = 9, 179
 g.append(text_graph_humidity_min)
 
-text_graph_humidity_med = label.Label(font_small, text='50', color=FOREGROUND_COLOR, scale=1)
-text_graph_humidity_med.anchor_point = 0.5, 0.0
-text_graph_humidity_med.anchored_position = 9, 153
-g.append(text_graph_humidity_med)
-
-text_graph_humidity_max = label.Label(font_small, text='100', color=FOREGROUND_COLOR, scale=1)
+# humidity y max field
+text_graph_humidity_max = label.Label(font_small, text=f'{humidity_y_max}', color=FOREGROUND_COLOR, scale=1)
 text_graph_humidity_max.anchor_point = 0.5, 0.0
 text_graph_humidity_max.anchored_position = 9, 127
 g.append(text_graph_humidity_max)
 
+# temperature x 12h field
 text_graph_temperature_x_12h = label.Label(font_small, text='12h', color=FOREGROUND_COLOR, scale=1)
 text_graph_temperature_x_12h.anchor_point = 1.0, 1.0
 text_graph_temperature_x_12h.anchored_position = 123, 120
 g.append(text_graph_temperature_x_12h)
 
+# temperature x 24h field
 text_graph_temperature_x_24h = label.Label(font_small, text='24h', color=FOREGROUND_COLOR, scale=1)
 text_graph_temperature_x_24h.anchor_point = 1.0, 1.0
 text_graph_temperature_x_24h.anchored_position = 198, 120
 g.append(text_graph_temperature_x_24h)
 
+# humidity x 12h field
 text_graph_humidity_x_12h = label.Label(font_small, text='12h', color=FOREGROUND_COLOR, scale=1)
 text_graph_humidity_x_12h.anchor_point = 1.0, 1.0
 text_graph_humidity_x_12h.anchored_position = 123, 199
 g.append(text_graph_humidity_x_12h)
 
+# humidity x 24h field
 text_graph_humidity_x_24h = label.Label(font_small, text='24h', color=FOREGROUND_COLOR, scale=1)
 text_graph_humidity_x_24h.anchor_point = 1.0, 1.0
 text_graph_humidity_x_24h.anchored_position = 198, 199
 g.append(text_graph_humidity_x_24h)
 
+# text counter field
 text_counter = label.Label(font_small, text=f'{counter}', color=FOREGROUND_COLOR, scale=1)
 text_counter.anchor_point = 0.0, 1.0
 text_counter.anchored_position = 4, 199
 g.append(text_counter)
 
+###############################################################
+# initialize the display
+displayio.release_displays()
+
+spi = busio.SPI(
+    board.IO5, # CLK pin
+    board.IO3, # MOSI pin
+    None) # MISO pin, not need to drive this display
+
+display_bus = displayio.FourWire(
+    spi,
+    command = board.IO9,
+    reset = board.IO11,
+    chip_select = board.IO7,
+    baudrate = 60000000) # try to use the higher SPI baudrate / clock, in the hope the display updates are faster and so less power is used by ESP32
+
+display = adafruit_ssd1681.SSD1681(display_bus, width = 200, height = 200, busy_pin = board.IO12, rotation = 0)
+###############################################################
+
+# finally, draw the full display
 display.show(g)
 display.refresh()
 
-# sleep for 180 seconds that is the min time of display refresh
-seconds_to_sleep = 180
-alarm_to_sleep = alarm.time.TimeAlarm(monotonic_time = time.monotonic() + seconds_to_sleep)
+# sleep for 600 seconds / 10 minutes
+# decrease the time it takes to get here since the start
+time_now = time.monotonic()
+seconds_to_sleep = 600 - time_now - time_start 
+alarm_to_sleep = alarm.time.TimeAlarm(seconds_to_sleep)
 alarm.exit_and_deep_sleep_until_alarms(alarm_to_sleep)
-# Does not return. Exits, and restarts after the sleep time.
-
+# does not return. Exits, and restarts after the sleep time.
--- firmware/sensor.py
+++ firmware/sensor.py
@@ -1,38 +1,49 @@
-import time
-import board
 import busio
 import adafruit_ahtx0
 import alarm
 
 class MemoryForSensorData():
-  def __init__(self, offset_address, capacity):
+  def __init__(self, offset_address, capacity_x2):
     self.memory = alarm.sleep_memory # from now own, use self.memory instead of alarm.sleep_memory
     self.offset_address_initial = self.offset_address = offset_address
-    self.capacity = capacity
+    self.capacity_x2 = capacity_x2
 
     if alarm.wake_alarm:
       # recover the values from the sleep memory
       self.current_size = (self.memory[self.offset_address] << 8) + self.memory[self.offset_address + 1]
-      self.offset_address += 2
-
       self.head_x2 = (self.memory[self.offset_address] << 8) + self.memory[self.offset_address + 1]
-      self.offset_address += 2
-
       self.tail_x2 = (self.memory[self.offset_address] << 8) + self.memory[self.offset_address + 1]
-      self.offset_address += 2
     else:
       # initial state of this variables
       self.current_size = 0
       self.head_x2 = 0
       self.tail_x2 = 0
-      self.offset_address += 6
 
+    self.offset_address = 6 # we use 6 bytes
     self.max_value = None
     self.min_value = None
 
-    self._update_variables()
+  @property
+  def sensor_memory_offset(self):
+    "Returns the total number of bytes used"
+    return self.offset_address + self.capacity_x2
+  
+  def add(self, value):  
+    # move head forward
+    if self.tail_x2 == self.head_x2 and (self.current_size * 2) == self.capacity_x2:
+      self.head_x2 = (self.head_x2 + 2) % self.capacity_x2
 
-  def _update_variables(self):
+    # add the data
+    self.memory[self.offset_address + self.tail_x2] = (value >> 8) & 0xff
+    self.memory[self.offset_address + self.tail_x2 + 1] = value & 0xff
+
+    # move tail forward (in step of 2)
+    self.tail_x2 = (self.tail_x2 + 2) % self.capacity_x2
+
+    # increase the current_size (only when ring buffer is not yet full)
+    if (self.current_size * 2) < self.capacity_x2:
+      self.current_size += 1
+
     # update the values on sleep memory
     self.memory[self.offset_address_initial] = (self.current_size >> 8) & 0xff
     self.memory[self.offset_address_initial + 1] = self.current_size & 0xff
@@ -44,34 +55,7 @@ class MemoryForSensorData():
     self.memory[self.offset_address_initial + 5] = self.tail_x2 & 0xff
 
   @property
-  def sensor_memory_offset(self):
-    "Returns the total number of bytes used"
-    return self.offset_address + (self.capacity * 2)
-  
-  def add(self, value):
-    if not isinstance(value, int):
-      raise TypeError("value must be a int")
-  
-    # move head forward
-    if self.tail_x2 == self.head_x2 and self.current_size == self.capacity:
-      self.head_x2 = (self.head_x2 + 2) % (self.capacity * 2)
-
-    # add the data
-    self.memory[self.offset_address + self.tail_x2] = (value >> 8) & 0xff
-    self.memory[self.offset_address + self.tail_x2 + 1] = value & 0xff
-
-    # move tail forward (in step of 2)
-    self.tail_x2 = (self.tail_x2 + 2) % (self.capacity * 2)
-
-    # increase the current_size (only when ring buffer is not yet full)
-    if self.current_size < self.capacity:
-      self.current_size += 1
-
-    self._update_variables()
-
-  @property
   def values(self):
-
     # check if ring buffer is empty
     if self.current_size < 1:
       return []
@@ -98,7 +82,7 @@ class MemoryForSensorData():
         self.min_value = data[i]
 
       # move head forward
-      head_x2 = (head_x2 + 2) % (self.capacity * 2)
+      head_x2 = (head_x2 + 2) % (self.capacity_x2 * 2)
 
     return data
   
@@ -115,19 +99,19 @@ class MemoryForSensorData():
     return self.min_value
 
 class Sensor(object):
-  def __init__(self, i2c_clk_pin, i2c_sda_pin):
+  def __init__(self, i2c_clk_pin, i2c_sda_pin, sleep_memory_offset):
     i2c = busio.I2C(i2c_clk_pin, i2c_sda_pin)
     self.temperature_humidity_sensor = adafruit_ahtx0.AHTx0(i2c)
     self.last_temperature = 0
     self.last_humidity = 0
-    self.data_sensor_temperature = MemoryForSensorData(128, 288) # reserve the first 128 bytes free for other usages. 144 points, 2 bytes for each point = 288
-    new_offset = self.data_sensor_temperature.sensor_memory_offset # this is the memory offset (memory used by previous MemoryForSensorData())
-    self.data_sensor_humidity = MemoryForSensorData(new_offset, 288)
+    self.data_sensor_temperature = MemoryForSensorData(sleep_memory_offset, 288) # 144 points, 2 bytes for each point = 288
+    sleep_memory_offset = self.data_sensor_temperature.sensor_memory_offset # this is the updated sleep memory offset
+    self.data_sensor_humidity = MemoryForSensorData(sleep_memory_offset, 288)
 
-  def run_periodic(self):
-    "Must be called every 5 minutes: will read and store the sensor data"
+  def read_and_store(self):
+    "Must be called to read the sensor data. Will store the sensor data in the sleep memory"
 
-    # read the sensor values
+    # read the sensor values and round the values
     self.last_temperature = round(self.temperature_humidity_sensor.temperature, 1)
     self.last_humidity = round(self.temperature_humidity_sensor.relative_humidity, 1)
 
@@ -145,7 +129,7 @@ class Sensor(object):
 
   @property
   def historic_data_temperature(self):
-    "Return an array of data"
+    "Return a list with temperature values, max, min and lenght"
     return self.data_sensor_temperature.values, \
       self.data_sensor_temperature.max, \
       self.data_sensor_temperature.min, \
@@ -153,9 +137,9 @@ class Sensor(object):
 
   @property
   def historic_data_humidity(self):
-    "Return an array of data"
+    "Return a list with humidity values, max, min and lenght"
     return self.data_sensor_humidity.values, \
       self.data_sensor_humidity.max, \
       self.data_sensor_humidity.min, \
       self.data_sensor_humidity.len
-
+  
